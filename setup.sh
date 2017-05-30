41ce28d57e45098823446562779e24aee21fd87a
Hirano Takanori <hrn.takanori@gmail.com>
2017-05-30 15:56:01 +0900

    インストールを選択できるように

--- setup.sh
+++ setup.sh
@@ -1,19 +1,54 @@
 #!/bin/sh
 
-DOT_FILES=(
-  .atom/config.cson
-  .atom/init.coffee
-  .atom/keymap.cson
-  .atom/snippets.cson
-  .atom/styles.less
-  .vimrc
-  .tmux.conf
-  .zshrc
-)
+CD=$(cd $(dirname $0) && pwd)
 
-apm install --packages-file packages.list
+case ${OSTYPE} in
+  darwin*)
+    if [ -x "'which brew'" ]; then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    PM_INSTALL="brew install"
+    ;;
+  linux*)
+    if [ -x "'which apt'" ]; then
+      PM_INSTALL="apt install"
+    elif [ -x "'which dnf'" ]; then
+      PM_INSTALL="dnf install"
+    elif [ -x "'which yum'" ]; then
+      PM_INSTALL="yum install"
+    fi
+    ;;
+esac
 
-for file in ${DOT_FILES[@]}
+for OPT in "$@"
 do
-  ln -s $HOME/git/dotfiles/$file $HOME/$file
+  case $OPT in
+    '-zsh' )
+      FLAG_ZSH=1
+      ;;
+    '-atom' )
+      FLAG_ATOM=1
+      ;;
+    '-vim' )
+      FLAG_VIM=1
+      ;;
+  esac
+  shift
 done
+
+if [ "$FLAG_ZSH" ]; then
+  ln -s $CD/.zshrc $HOME/.zshrc
+fi
+
+if [ "$FLAG_ATOM" ]; then
+  apm install --packages-file packages.list
+  ln -s $CD/.atom/config.cson $HOME/.atom/config.cson
+  ln -s $CD/.atom/init.coffee $HOME/.atom/init.coffee
+  ln -s $CD/.atom/keymap.cson $HOME/.atom/keymap.cson
+  ln -s $CD/.atom/snippets.cson $HOME/.atom/snippets.cson
+  ln -s $CD/.atom/styles.less $HOME/.atom/styles.less
+fi
+
+if [ "$FLAG_VIM" ]; then
+  ln -s $CD/.vimrc $HOME/.vimrc
+fi
