ddd63244c763c05a0f010802b760da292a7f5f50
Takanori Hirano <hrn.takanori@gmail.com>
2018-06-30 14:20:11 +0900

    :wrench:

--- hosts.sh
+++ hosts.sh
@@ -1,2 +1,40 @@
-#!/bin/sh
-curl -s ${URL}
+#!/bin/bash
+set -eu
+
+HOST=${HOST:-localhost}
+BASE=${BASE:-true}
+CUI=${CUI:-false}
+GUI=${GUI:-false}
+CONNECTION=
+[ ${HOST} = "localhost" ] && CONNECTION="local" || CONNECTION="ssh"
+# echo "Target host: ${HOST}"
+# echo "Base: ${BASE}"
+# echo "CUI: ${CUI}"
+# echo "GUI: ${GUI}"
+# echo "Connection: ${CONNECTION}"
+
+json=`cat << EOS
+{
+  "local": {
+    "hosts": [
+      "${HOST}"
+    ],
+    "vars": {
+      "ansible_connection": "${CONNECTION}",
+      "config_dotfiles": {
+        "base": ${BASE},
+        "cui": ${CUI},
+        "gui": ${GUI}
+      },
+      "config_install": {
+        "base": ${BASE},
+        "cui": ${CUI},
+        "gui": ${GUI}
+      }
+    }
+  }
+}
+EOS
+`
+
+echo "${json}"
