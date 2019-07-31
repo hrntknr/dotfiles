8d40bc136fa2f4716cf0e3ea9850854c7736b6ac
Takanori Hirano <hrn.takanori@gmail.com>
2019-07-31 17:54:08 +0900

    :wrench:

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ for file in $(find files -maxdepth 1 -type f); do
   srcFile=$(basename $file)
   dstFile=$HOME/${srcFile//@/\/}
   if [ ! -e dstFile ];then
-    mkdir -p dirname $dstFile
+    mkdir -p $(dirname $dstFile)
   fi
   cp files/$srcFile $(dirname $dstFile)
 done
