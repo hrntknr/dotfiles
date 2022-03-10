73c5c3ed538a6a5f6b67374c4762ea5718f97eba
Takanori Hirano <hrn.takanori@gmail.com>
2022-03-11 00:58:52 +0900

    wez

--- files/.config@wezterm@wezterm.lua
+++ files/.config@wezterm@wezterm.lua
@@ -1,13 +1,19 @@
 local wezterm = require 'wezterm';
 
 return {
+  font = wezterm.font_with_fallback({
+    "Firge35 Console",
+  }),
+  font_dirs = {"fonts"},
+  font_size = 11,
   use_ime = true,
-  use_fancy_tab_bar=false,
-  font_size = 11.5,
-  hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
-  window_background_opacity = 0.9,
+
+  use_fancy_tab_bar=false,
+  hide_tab_bar_if_only_one_tab = true,
+
   enable_wayland = true,
+  window_background_opacity = 0.9,
   window_decorations = "RESIZE",
   window_padding = {
     left = 4,
@@ -15,9 +21,10 @@ return {
     top = 0,
     bottom = 0,
   },
-  color_scheme = "OneHalfDark",
-  exit_behavior = "Close",
   window_close_confirmation = "NeverPrompt",
+  exit_behavior = "Close",
+
+  color_scheme = "OneHalfDark",
   colors = {
     foreground = "white",
     background = "black",
