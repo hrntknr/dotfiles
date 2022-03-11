b18170784a531eefc1c89d67973233fecdf9dfb1
Takanori Hirano <hrn.takanori@gmail.com>
2022-03-11 20:36:40 +0900

    alacritty

--- setup.sh
+++ setup.sh
@@ -2,12 +2,16 @@
 set -eu
 cd $(dirname $0)
 
-for file in $(find files -maxdepth 1 -type f); do
-  srcFile=$(basename $file)
-  dstFile=$HOME/${srcFile//@/\/}
+function copyfile {
+  file=$1
+  srcFile=$(basename "$file")
+  dstFile="$HOME/${srcFile//@/\/}"
   echo "copy src:$srcFile dst:$dstFile"
-  if [ ! -e dstFile ];then
-    mkdir -p $(dirname $dstFile)
+  if [ ! -d $(dirname "$dstFile") ]; then
+    mkdir -p $(dirname "$dstFile")
   fi
-  cp files/$srcFile $dstFile
-done
+  cp "files/$srcFile" "$dstFile"
+}
+
+export -f copyfile
+find files -maxdepth 1 -type f -exec bash -c 'copyfile "$0"' {} \;
