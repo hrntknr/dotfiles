6b0d338af878cdfcd5ed13e3a44f5f7b9731a9f0
Takanori Hirano <me@hrntknr.net>
2023-11-17 18:49:22 +0900

    Update .config@nvim@init.lua

--- files/.config@nvim@init.lua
+++ files/.config@nvim@init.lua
@@ -75,14 +75,6 @@ require("lazy").setup({
             },
           },
         })
-        vim.api.nvim_create_autocmd("UiEnter", {
-          pattern = { "*" },
-          callback = function()
-            if vim.fn.argc() == 0 then
-              vim.cmd "Neotree toggle"
-            end
-          end,
-        })
       end,
     },
     {
