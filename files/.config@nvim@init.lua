a0c4a8fc7c8c394030c02ce04b61cb2557ad3aa2
Takanori Hirano <me@hrntknr.net>
2023-11-20 14:48:05 +0900

    Update .config@nvim@init.lua

--- files/.config@nvim@init.lua
+++ files/.config@nvim@init.lua
@@ -255,6 +255,13 @@ require("lazy").setup({
         vim.g.copilot_no_tab_map = true
         vim.g.copilot_assume_mapped = true
         vim.g.copilot_tab_fallback = ""
+        vim.g.copilot_filetypes = {
+          yaml = true,
+          markdown = true,
+          help = true,
+          gitcommit = true,
+          gitrebase = true,
+        }
       end
     },
     "editorconfig/editorconfig-vim",
