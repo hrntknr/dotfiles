fb719b70c70b06601973b14362acefa7006d2f0e
Takanori Hirano <me@hrntknr.net>
2025-03-22 02:13:52 +0900

    git crypt

--- setup.sh
+++ setup.sh
@@ -32,6 +32,28 @@ for file in $(find $cur/files -type f); do
   fi
   cp -v "$cur/files/$file" "$target"
 done
+if type git-crypt >/dev/null 2>&1; then
+  set +e
+  (
+    set -e
+    cd $cur
+    git crypt unlock
+  )
+  if [ $? -eq 0 ]; then
+    set -e
+    for file in $(find $cur/files-crypt -type f); do
+      file=${file#$cur/files/}
+      target="$basedir/$file"
+      dir=$(dirname "$target")
+      if [ ! -d "$dir" ]; then
+        mkdir -p "$dir"
+      fi
+      ln -sfv "$cur/files-crypt/$file" "$target"
+    done
+  else
+    echo "Skip files-crypt"
+  fi
+fi
 
 if [ ! -e "$basedir/.zsh/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions $basedir/.zsh/zsh-autosuggestions
